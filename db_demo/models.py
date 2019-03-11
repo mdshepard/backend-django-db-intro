86579dbfc25d79c5f9de3e74e36e309512518625
mdshepard <mdshepard8@gmail.com>
2019-03-11 14:58:49 -0400

    implemented models into models.py

--- db_demo/models.py
+++ db_demo/models.py
@@ -5,3 +5,59 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Your models go here!
+
+
+class Profile(models.Model):
+    avatar = models.CharField(max_length=45)
+    body = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.body
+
+
+class Roles(models.Model):
+    title = models.CharField(max_length=45)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Categories(models.Model):
+    title = models.CharField(max_length=50)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    parent_id = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
+
+class Contributors(models.Model):
+    role = models.ForeignKey(Roles, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Permissions(models.Model):
+    title = models.CharField(max_length=45)
+
+    def __str__(self):
+        return self.title
+
+
+class Tags(models.Model):
+    body = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.body
+
+
+class Pages(models.Model):
+    title = models.CharField(max_length=50)
+    body = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    catagory = models.ForeignKey(Categories, on_delete=models.CASCADE)
+    is_published = models.SmallIntegerField()
+    is_flagged = models.SmallIntegerField()
+    tags = models.ForeignKey(Tags, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
